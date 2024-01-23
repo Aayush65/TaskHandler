0d2fb2c09e538b4c9cd9361646cfa6dd26bc2740
Aayush Gupta <cdtaayushgupta@gmail.com>
2024-01-24 00:34:17 +0530

    Added user adding endpoint

--- src/models/Users.ts
+++ src/models/Users.ts
@@ -5,8 +5,14 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     // id is redundant here, because mongodb creates an _id for each document (entry)
 
-    phone_number: Number,
+    phone_number: {
+        type: Number,
+        unique: true,
+    },
     priority: Number,
+
+    // Adding pass to help in authentication and issuing JWT tokens
+    pass: String,
 });
 
 const UserModel = mongoose.model("User", userSchema);
