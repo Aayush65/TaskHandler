9f83e0297c98fb54cb6c0c7aad5738cb6d0af95c
Aayush Gupta <cdtaayushgupta@gmail.com>
2024-01-24 00:34:32 +0530

    Updated views

--- src/views/index.ts
+++ src/views/index.ts
@@ -1,26 +1,30 @@
 import { Response } from 'express'; 
 
+export function statusOkay(res: Response, message: any) {
+    res.status(200).json(message);
+}
+
+export function created(res: Response, message: any) {
+    res.status(201).json(message);
+}
+
 export function badRequest(res: Response) {
     res.status(400).json({message: 'Bad Request'});
 }
 
 export function unauthAccess(res: Response) {
-    res.status(401).json({message: 'Unauthorised Access'});
+    res.status(401).json({message: 'Unauthorised'});
 }
 
-export function wrongCredentials(res: Response) {
-    res.status(403).json({message: 'Wrong Credentials'});
-}
-
-export function serverError(res: Response, err: unknown) {
-    console.log(err);
-    res.status(500).json({message: 'Internal Server Error'});
+export function forbidden(res: Response) {
+    res.status(403).json({message: 'Forbidden'});
 }
 
 export function notFound(res: Response) {
     res.status(404).json({message: 'Not Found'});
 }
 
-export function statusOkay(res: Response, message: any) {
-    res.status(200).json(message);
+export function serverError(res: Response, err: unknown) {
+    console.log(err);
+    res.status(500).json({message: 'Internal Server Error'});
 }
