2d0f31541ded15291fca8bbdf70003a57dbc9a74
Aayush Gupta <cdtaayushgupta@gmail.com>
2024-01-27 00:20:54 +0530

    Error Handling

--- src/jobs/dailyReminder.ts
+++ src/jobs/dailyReminder.ts
@@ -5,37 +5,41 @@ import UserModel from "../models/Users";
 
 
 export default async function dailyReminders() {
-    const priorityMap: { [key: number]: number } = {0: 0, 2: 1, 4: 2}
+    try {
+        const priorityMap: { [key: number]: number } = {0: 0, 2: 1, 4: 2}
 
-    const pipeline = [
-        { $project: { user_id: 1, leftDays: { $ceil: { $divide: [{ $subtract: [{ $toDate: "$due_date" }, new Date()] }, 86400000] } } } },
-        { $match: { leftDays: { $in: Object.keys(priorityMap).map((e) => parseInt(e)) } } },
-    ]
+        const pipeline = [
+            { $project: { user_id: 1, leftDays: { $ceil: { $divide: [{ $subtract: [{ $toDate: "$due_date" }, new Date()] }, 86400000] } } } },
+            { $match: { leftDays: { $in: Object.keys(priorityMap).map((e) => parseInt(e)) } } },
+        ]
 
-    const tasksToUpdate = await TaskModel.aggregate(pipeline);
+        const tasksToUpdate = await TaskModel.aggregate(pipeline);
 
-    const bulkOperations = tasksToUpdate.map(task => ({
-        updateOne: {
-            filter: { _id: new ObjectId(task._id) },
-            update: { $set: { priority: priorityMap[task.leftDays as number] } }
-        }
-    }));
+        const bulkOperations = tasksToUpdate.map(task => ({
+            updateOne: {
+                filter: { _id: new ObjectId(task._id) },
+                update: { $set: { priority: priorityMap[task.leftDays as number] } }
+            }
+        }));
 
-    await TaskModel.bulkWrite(bulkOperations);
+        await TaskModel.bulkWrite(bulkOperations);
 
-    const uniqueUsers: { [key: string]: number }  = {};
-    tasksToUpdate.forEach(element => {
-       uniqueUsers[element.user_id] = element.leftDays; 
-    });
+        const uniqueUsers: { [key: string]: number }  = {};
+        tasksToUpdate.forEach(element => {
+        uniqueUsers[element.user_id] = element.leftDays; 
+        });
 
-    const pipeline2 = [
-        { $match: { _id: { $in: Object.keys(uniqueUsers).map((e) => new ObjectId(e)) } } },
-        { $project: { _id: { $toString: "$_id" }, phone_number: 1 } },
-    ]
+        const pipeline2 = [
+            { $match: { _id: { $in: Object.keys(uniqueUsers).map((e) => new ObjectId(e)) } } },
+            { $project: { _id: { $toString: "$_id" }, phone_number: 1 } },
+        ]
 
-    const allPhones = await UserModel.aggregate(pipeline2);
-    allPhones.forEach(element => {
-        makeCall(element.phone_number, "Complete your tasks quicky");
-        // console.log(element.phone_number, "Complete your tasks quicky");
-    })
+        const allPhones = await UserModel.aggregate(pipeline2);
+        allPhones.forEach(element => {
+            makeCall(element.phone_number, "Complete your tasks quicky");
+            // console.log(element.phone_number, "Complete your tasks quicky");
+        })
+    } catch(err) {
+        console.log(err);
+    }
 }
--- src/utils/makeCall.ts
+++ src/utils/makeCall.ts
@@ -1,11 +1,15 @@
 import { Twilio } from 'twilio';
 
 export default async function makeCall(recipient: string, message: string) {
-    const client = new Twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
-    client.calls.create({
-        twiml: `<Response><Say>${message}</Say></Response>`,
-        to: recipient,
-        from: process.env.MY_NUMBER!,
-    })
-        .then((message) => console.log(message.sid));
+    try {    
+        const client = new Twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+        client.calls.create({
+            twiml: `<Response><Say>${message}</Say></Response>`,
+            to: recipient,
+            from: process.env.MY_NUMBER!,
+        })
+            .then((message) => console.log(message.sid));
+    } catch(err) {
+        console.log(err);
+    }
 }
