22850b869dc3adbc14dd1fe3ff76aec4510e6686
Aayush Gupta <cdtaayushgupta@gmail.com>
2024-01-26 18:59:05 +0530

    Added Calling feature from Twilio

---
+++ src/utils/makeCall.ts
@@ -0,0 +1,11 @@
+import { Twilio } from 'twilio';
+
+export default async function makeCall(recipient: string, message: string) {
+    const client = new Twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+    client.calls.create({
+        twiml: `<Response><Say>${message}</Say></Response>`,
+        to: recipient,
+        from: process.env.MY_NUMBER!,
+    })
+        .then((message) => console.log(message.sid));
+}
